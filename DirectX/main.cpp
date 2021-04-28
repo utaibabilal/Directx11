73b51accf94bc7d39fc170a193c37972a2b93642
utaibabilal <mbk.ciit.bilalkhan@gmail.com>
2021-04-29 01:00:22 +0500

    Uptil first triangle rendering

--- DirectX/main.cpp
+++ DirectX/main.cpp
@@ -27,23 +27,33 @@ int colormodg = 1;
 int colormodb = 1;
 
 ///////////////**************new**************////////////////////
+ID3D11Buffer* triangleVertBuffer;
+ID3D11VertexShader* VS;
+ID3D11PixelShader* PS;
+ID3D10Blob* VS_Buffer;
+ID3D10Blob* PS_Buffer;
+ID3D11InputLayout* vertLayout;
+///////////////**************new**************////////////////////
 
 LPCTSTR WndClassName = "firstwindow";
 HWND hwnd = NULL;
+HRESULT hr;
 
 const int Width  = 500;
 const int Height = 500;
 
-///////////////**************new**************////////////////////
+
 
 //Function Prototypes//
 bool InitializeDirect3d11App(HINSTANCE hInstance);
 void ReleaseObjects();
-bool InitScene();
+bool InitTriangle();
 void UpdateScene();
 void DrawScene();
+void SetViewPort();
+void CompileShaders();
 
-///////////////**************new**************////////////////////
+
 
 bool InitializeWindow(HINSTANCE hInstance,
     int ShowWnd,
@@ -57,20 +67,41 @@ LRESULT CALLBACK WndProc(HWND hWnd,
     LPARAM lParam);
 
 
-int WINAPI WinMain(HINSTANCE hInstance,    //Main windows function
-    HINSTANCE hPrevInstance, 
-    LPSTR lpCmdLine,
-    int nShowCmd)
+//Vertex Structure and Vertex Layout (Input Layout)//
+struct Vertex    //Overloaded Vertex Structure
+{
+     Vertex(){}
+    Vertex(float x, float y, float z,
+        float cr, float cg, float cb, float ca)
+        : pos(x,y,z), color(cr, cg, cb, ca){}
+
+    XMFLOAT3 pos;
+    XMFLOAT4 color;
+};
+
+D3D11_INPUT_ELEMENT_DESC layout[] =
+{
+   // { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+	{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },  
+    { "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },  
+
+
+};
+UINT numElements = ARRAYSIZE(layout);
+///////////////**************new**************////////////////////
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)  // main function 
 {
 
-    if(!InitializeWindow(hInstance, nShowCmd, Width, Height, true))
+    if(!InitializeWindow(hInstance, nShowCmd, Width, Height, true))   // init the window
     {
         MessageBox(0, "Window Initialization - Failed",
             "Error", MB_OK);
         return 0;
     }
 
-///////////////**************new**************////////////////////
+
 
     if(!InitializeDirect3d11App(hInstance))    //Initialize Direct3D
     {
@@ -79,7 +110,10 @@ int WINAPI WinMain(HINSTANCE hInstance,    //Main windows function
         return 0;
     }
 
-    if(!InitScene())    //Initialize our scene
+	CompileShaders();  // compile shaders from the file
+	SetViewPort();     // setting of view port
+
+    if(!InitTriangle())    //Initialize our scene
     {
         MessageBox(0, "Scene Initialization - Failed",
             "Error", MB_OK);
@@ -90,15 +124,12 @@ int WINAPI WinMain(HINSTANCE hInstance,    //Main windows function
 
     ReleaseObjects();
     
-///////////////**************new**************////////////////////
+
 
     return 0;
 }
 
-bool InitializeWindow(HINSTANCE hInstance,
-    int ShowWnd,
-    int width, int height,
-    bool windowed)
+bool InitializeWindow(HINSTANCE hInstance, int ShowWnd, int width, int height, bool windowed)
 {
     typedef struct _WNDCLASS {
         UINT cbSize;
@@ -162,7 +193,7 @@ bool InitializeWindow(HINSTANCE hInstance,
     return true;
 }
 
-///////////////**************new**************////////////////////
+
 
 bool InitializeDirect3d11App(HINSTANCE hInstance)
 {
@@ -218,42 +249,126 @@ void ReleaseObjects()   //to remove the resources
     SwapChain->Release();
     d3d11Device->Release();
     d3d11DevCon->Release();
+	 triangleVertBuffer->Release();
+    VS->Release();
+    PS->Release();
+    VS_Buffer->Release();
+    PS_Buffer->Release();
+    vertLayout->Release();
 }
-bool InitScene()
-{
 
+void CompileShaders()
+{
+	 //Compile Shaders from shader file
+    hr = D3DX11CompileFromFile("Effects.fx", 0, 0, "VS", "vs_4_0", 0, 0, 0, &VS_Buffer, 0, 0);
+    hr = D3DX11CompileFromFile("Effects.fx", 0, 0, "PS", "ps_4_0", 0, 0, 0, &PS_Buffer, 0, 0);
+
+    //Create the Shader Objects
+    hr = d3d11Device->CreateVertexShader(VS_Buffer->GetBufferPointer(), VS_Buffer->GetBufferSize(), NULL, &VS);
+    hr = d3d11Device->CreatePixelShader(PS_Buffer->GetBufferPointer(), PS_Buffer->GetBufferSize(), NULL, &PS);
+
+    //Set Vertex and Pixel Shaders
+    d3d11DevCon->VSSetShader(VS, 0, 0);   // this 0 in num of class instances
+    d3d11DevCon->PSSetShader(PS, 0, 0);   // "
+}
+bool InitTriangle()
+{
+	 
+
+    //Create the vertex buffer
+    Vertex v[] =
+    {
+        Vertex( 0.0f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f ),
+        Vertex( 0.5f, -0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 1.0f ),
+        Vertex( -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 1.0f ), 
+       
+       
+    };
+
+    D3D11_BUFFER_DESC vertexBufferDesc;
+    ZeroMemory( &vertexBufferDesc, sizeof(vertexBufferDesc) );
+
+    vertexBufferDesc.Usage          = D3D11_USAGE_DEFAULT;
+    vertexBufferDesc.ByteWidth      = sizeof( Vertex ) * 3;
+    vertexBufferDesc.BindFlags      = D3D11_BIND_VERTEX_BUFFER;
+    vertexBufferDesc.CPUAccessFlags = 0;
+    vertexBufferDesc.MiscFlags      = 0;
+
+    D3D11_SUBRESOURCE_DATA vertexBufferData; 
+
+    ZeroMemory( &vertexBufferData, sizeof(vertexBufferData) );
+    vertexBufferData.pSysMem = v;
+
+	/*1-> ptr to buffer desc
+	2-> address of subresource. 
+	3-> is the returned buffer*/
+    hr = d3d11Device->CreateBuffer( &vertexBufferDesc, &vertexBufferData, &triangleVertBuffer);  /// creating buffer   
+
+    //Set the vertex buffer
+    UINT stride = sizeof( Vertex );
+    UINT offset = 0;
+
+	//Create the Input Layout
+
+	/*1-> layout
+	2-> num of elements
+	3->ptr to start of VS
+	4->size
+	5->returned ptr*/
+
+    d3d11Device->CreateInputLayout( layout, numElements, VS_Buffer->GetBufferPointer(), VS_Buffer->GetBufferSize(), &vertLayout );  // creation of input layout
+
+
+	// binding  vertexbuffer to input assembeler
+	/* 1-> input slot
+		2-> num of buffer to bind
+		3-> ptr to actual buffer.
+		4- size of each vertex
+		5-> ptr to start*/
+    d3d11DevCon->IASetVertexBuffers( 0, 1, &triangleVertBuffer, &stride, &offset );
+	// binding of our layout  to input assembeler
+    d3d11DevCon->IASetInputLayout( vertLayout );
+
+    //Setting topology Primitive Topology
+    d3d11DevCon->IASetPrimitiveTopology( D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST );
+ 
     return true;
+}
+
+void SetViewPort()
+{
+	//Create the Viewport
+    D3D11_VIEWPORT viewport;
+    ZeroMemory(&viewport, sizeof(D3D11_VIEWPORT));
+
+    viewport.TopLeftX = 0;
+    viewport.TopLeftY = 0;
+    viewport.Width = Width ;
+    viewport.Height = Height ;
+
+	 //Set the Viewport
+    d3d11DevCon->RSSetViewports(1, &viewport);
 }
 
 void UpdateScene()
 {
-    //Update the colors of our scene
-    red += colormodr * 0.00005f;
-    green += colormodg * 0.00002f;
-    blue += colormodb * 0.00001f;
-
-    if(red >= 1.0f || red <= 0.0f)
-        colormodr *= -1;
-    if(green >= 1.0f || green <= 0.0f)
-        colormodg *= -1;
-    if(blue >= 1.0f || blue <= 0.0f)
-        colormodb *= -1;
+   
 }
-
 void DrawScene() // main render
 {
+	 /// Any thing to be called is between clear render target view and present
     //Clear our backbuffer to the updated color
-    D3DXCOLOR bgColor( red, green, blue, 1.0f );
-
+     float bgColor[4] = {(0.0f, 0.0f, 1.0f, 0.0f)};
     d3d11DevCon->ClearRenderTargetView(renderTargetView, bgColor);
 
+    //Draw the triangle
+	/*1-> num of vertex
+	2->start of vertex*/
+    d3d11DevCon->Draw( 3, 0 );
     //Present the backbuffer to the screen
     SwapChain->Present(0, 0);
 }
-
-///////////////**************new**************////////////////////
-
-int messageloop(){
+int  messageloop(){
     MSG msg;
     ZeroMemory(&msg, sizeof(MSG));
     while(true)
@@ -273,23 +388,17 @@ int messageloop(){
             TranslateMessage(&msg);    
             DispatchMessage(&msg);
         }
-        else{
-///////////////**************new**************////////////////////
-            // run game code
-            
-            UpdateScene();
-            DrawScene();
-            
-///////////////**************new**************////////////////////
+        else  // if there no interupt 
+		{
+			// run game code 
+			UpdateScene();
+			DrawScene();
         }
     }
     return msg.wParam;
 }
 
-LRESULT CALLBACK WndProc(HWND hwnd,
-    UINT msg,
-    WPARAM wParam,
-    LPARAM lParam)
+LRESULT CALLBACK WndProc(HWND hwnd,UINT msg, WPARAM wParam,LPARAM lParam)
 {
     switch( msg )
     {
@@ -303,8 +412,5 @@ LRESULT CALLBACK WndProc(HWND hwnd,
         PostQuitMessage(0);
         return 0;
     }
-    return DefWindowProc(hwnd,
-        msg,
-        wParam,
-        lParam);
+    return DefWindowProc(hwnd, msg, wParam, lParam);
 }
